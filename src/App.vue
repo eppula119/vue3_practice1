12641082021ff44767a10d9f13ee607b8a67eaee
noguchi <noguchi@electrodream.co.jp>
2023-01-01 11:22:09 +0900

    テストコミット

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  let message = 'Hello World'
+
+  const upperCase = () => {
+    message = message.toUpperCase()
+  }
+
+  upperCase()
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Vue 3 入門練習</h1>
+  <p v-text="message"></p>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
