94164559d5e4cb6824b142486ca1c61ba4e73786
eppula119 <firstdouga@gmail.com>
2023-01-07 22:16:54 +0900

    テスト

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <script setup>
-import { ref, watchEffect } from 'vue';
+import Hello from './components/HelloWorld.vue';
+import { ref } from 'vue';
 
-const count = ref(0);
-const count2 = ref(100);
-watchEffect(() => console.log(`${count.value}/${count2.value}`));
+const name = ref('John');
+const changeName = () => {
+  name.value = 'ssss'
+}
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <button @click="count++">Count:{{ count }}</button>
-  <button @click="count2++">Count2:{{ count2 }}</button>
+  <Hello :name="name" />
+  <button @click="changeName">Change Name</button>
 </template>
 
-
 <style>
-  .active {
-    color: red;
-    font-weight: 900
-  }
-  .underLine {
-    text-decoration: underline;
-  }
-  .back {
-    background-color: black;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,15 @@
 <script setup>
-import { ref } from 'vue'
+const props = defineProps({
+  name: String,
+});
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+const changeName = () => {
+  props.name = 'Ken';
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h2>子コンポーネント</h2>
+  <p>Hello {{ props.name }}</p>
+  <button @click="changeName">Change sName</button>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
