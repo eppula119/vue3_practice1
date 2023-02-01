c513e48451de56a712422923b609cb3d6dd090ea
eppula119 <firstdouga@gmail.com>
2023-02-01 23:04:11 +0900

    テスト

--- src/components/CompA.vue
+++ src/components/CompA.vue
@@ -3,7 +3,7 @@ import CompB from './CompB.vue';
 import { counter } from '../store/countStore';
 </script>
 <template>
-  <h2>CompAコンポーネン</h2>
+  <h2>CompAコンポーネ</h2>
   <p>Count:{{ counter.count }}</p>
   <CompB />
 </template>
